2d6a31b6fa2ebdd816d087ae9b0495819cecaf30
Rakhmat Nashrul Wathon <rakhmat@mhcasia.com>
2020-05-02 01:51:41 +0700

    Updated the default layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
-
 import { rhythm, scale } from "../utils/typography"
+import {Navbar, Nav, Container, Jumbotron} from "react-bootstrap"
 
 const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
@@ -9,50 +9,80 @@ const Layout = ({ location, title, children }) => {
 
   if (location.pathname === rootPath) {
     header = (
-      <h1>
-        <Link
-          style={{
-            boxShadow: `none`,
-            color: `inherit`,
-          }}
-          to={`/`}
-        >
-          {title}
-        </Link>
-      </h1>
+      <div className="header" style={{minHeight: "350px"}}>
+        <Container>
+          <Navbar bg="transparent">
+              <h1 className="header-title">
+                <Link
+                  style={{
+                    boxShadow: `none`,
+                    color: `inherit`,
+                  }}
+                  to={`/`}
+                >
+                  {title}
+                </Link>
+              </h1>
+              <Navbar.Collapse className="justify-content-end">
+                <Nav>
+                  <Nav.Link href="#" className="text-white">Blog</Nav.Link>
+                  <Nav.Link href="#" className="text-white">About</Nav.Link>
+                </Nav>
+              </Navbar.Collapse>  
+          </Navbar>
+          <Jumbotron className="bg-transparent">
+            <h2 className="header-text">
+              Your time is limited, so don't waste it living someone else's life. Don't be trapped by Dogma which is living with the result of other people's thinking.
+            </h2>
+          </Jumbotron>
+        </Container>
+      </div>
     )
   } else {
     header = (
-      <h3>
-        <Link
-          style={{
-            boxShadow: `none`,
-            color: `inherit`,
-          }}
-          to={`/`}
-        >
-          {title}
-        </Link>
-      </h3>
+      <div className="header">
+        <Navbar bg="transparent">
+          <Container>
+            <h3 className="header-title">
+              <Link
+                style={{
+                  boxShadow: `none`,
+                  color: `inherit`,
+                }}
+                to={`/`}
+              >
+                {title}
+              </Link>
+            </h3>
+            <Navbar.Collapse className="justify-content-end">
+              <Nav>
+                <Nav.Link href="#" className="text-white">Blog</Nav.Link>
+                <Nav.Link href="#" className="text-white">About</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>  
+          </Container>
+        </Navbar>
+      </div>
     )
   }
   return (
-    <div
-      style={{
-        marginLeft: `auto`,
-        marginRight: `auto`,
-        maxWidth: rhythm(32),
-        padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
-      }}
-    >
+    <>
       <header>{header}</header>
-      <main>{children}</main>
-      <footer>
-        © {new Date().getFullYear()}, Built with
-        {` `}
-        <a href="https://www.gatsbyjs.org">Gatsby</a>
-      </footer>
-    </div>
+      <Container
+        style={{
+          marginLeft: `auto`,
+          marginRight: `auto`,
+          padding: `${rhythm(1.5)} ${rhythm(3 / 4)}`,
+        }}
+      >
+        <main>{children}</main>
+        <footer>
+          © {new Date().getFullYear()}, Built with
+          {` `}
+          <a href="https://www.gatsbyjs.org">Gatsby</a>
+        </footer>
+      </Container>
+    </>
   )
 }
 
