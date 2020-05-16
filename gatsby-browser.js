0263c75fa285b2b0223e720421271aa71eca5670
rnwathon <rakhmatnashrul@gmail.com>
2020-05-16 15:27:32 +0700

    Changed the prism theme

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,7 +2,7 @@
 import "typeface-roboto"
 import "typeface-nunito"
 
-import "prismjs/themes/prism.css"
+import "prismjs/themes/prism-okaidia.css"
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
