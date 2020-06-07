08586b4c54e613a32cdca397eee53720ddd514b8
rnwathon <rakhmatnashrul@gmail.com>
2020-06-07 08:33:59 +0800

    Updated the header design

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,14 +4,17 @@ import { rhythm } from "../utils/typography"
 import {Navbar, Nav, Container, Jumbotron} from "react-bootstrap"
 import { useStaticQuery, graphql } from "gatsby"
 import Image from "gatsby-image"
+import Memphis0 from '../../content/assets/memphis0.svg'
+import Memphis1 from '../../content/assets/memphis1.svg'
+// import Potrait from '../../content/assets/potrait.png'
 
 const Header = ({location, title}) => {
   const data = useStaticQuery(graphql`
     query {
-      avatar: file(absolutePath: { regex: "/profile-pic.jpg/" }) {
+      avatar: file(absolutePath: { regex: "/potrait.png/" }) {
         childImageSharp {
-          fixed(width: 50, height: 50) {
-            ...GatsbyImageSharpFixed
+          fluid {
+            ...GatsbyImageSharpFluid
           }
         }
       }
@@ -32,87 +35,52 @@ const Header = ({location, title}) => {
   const rootPath = `${__PATH_PREFIX__}/`
   if (location.pathname === rootPath) {
     return (
-      <div className="header" style={{minHeight: "350px"}}>
+      <div className="header">
+        <Memphis0 className="memphis0"/>
+        <Memphis1 className="memphis1"/>
         <Container>
           <Navbar bg="transparent">
-            <Image
-              fixed={data.avatar.childImageSharp.fixed}
-              style={{
-                marginRight: rhythm(1 / 2),
-                marginBottom: 0,
-                minWidth: 50,
-                borderRadius: `100%`,
-              }}
-              imgStyle={{
-                borderRadius: `50%`,
-              }}
-            />
-              <h1 className="header-title">
-                <Link
-                  style={{
-                    boxShadow: `none`,
-                    color: `inherit`,
-                  }}
-                  to={`/`}
-                >
-                  {title}
-                </Link>
-              </h1>
-              <Navbar.Collapse className="justify-content-end">
-                <Nav>
-                  <Link to="/blog" className="text-white mx-3">Blog</Link>
-                  <Link to="/about" className="text-white mx-3">About</Link>
-                </Nav>
-              </Navbar.Collapse>  
+              <Navbar.Brand>
+                <Link to={`/`}> {title} </Link>
+              </Navbar.Brand>
+              <Nav className="ml-auto">
+                <Link to="/blog">Blog</Link>
+                <Link to="#">Resume</Link>
+              </Nav>
           </Navbar>
-          <Jumbotron className="bg-transparent">
-            <h2 className="header-text">
-              Your time is limited, so don't waste it living someone else's life. Don't be trapped by Dogma which is living with the result of other people's thinking ✨
-            </h2>
-            <p>
-              - Steve Jobs
-            </p>
-          </Jumbotron>
+          <section className="display">
+            <div className="display-text">
+              <h2> Hi 👋🏼 </h2>
+              <h2> I'm Rakhmat </h2>
+              <h3> Front-End Developer </h3>
+            </div>
+            <div className="display-image">
+              <Image
+                fluid={data.avatar.childImageSharp.fluid}
+                style={{
+                  width: "100%",
+                  height: "100%"
+                }}
+              />
+            </div>
+          </section>
         </Container>
       </div>
     )
   } else {
     return (
       <div className="header">
+        <Memphis0 className="memphis0"/>
+        <Memphis1 className="memphis1"/>
         <Container>
           <Navbar bg="transparent">
-            <Link
-              style={{
-                boxShadow: `none`,
-                color: `inherit`,
-                display: `flex`,
-                alignItems: `center`
-              }}
-              to={`/`}
-            >
-              <Image
-                fixed={data.avatar.childImageSharp.fixed}
-                style={{
-                  marginRight: rhythm(1 / 2),
-                  marginBottom: 0,
-                  minWidth: 50,
-                  borderRadius: `100%`,
-                }}
-                imgStyle={{
-                  borderRadius: `50%`,
-                }}
-              />
-              <h3 className="header-title">
-                {title}
-              </h3>
-            </Link>
-
-            <Navbar.Collapse className="justify-content-end">
-              <Nav>
-                <Link to="/blog" className="text-white mx-3">Blog</Link>
-                <Link to="/about" className="text-white mx-3">About</Link>
+              <Navbar.Brand>
+                <Link to={`/`}> {title} </Link>
+              </Navbar.Brand>
+              <Nav className="ml-auto">
+                <Link to="/blog">Blog</Link>
+                <Link to="#">Resume</Link>
               </Nav>
-            </Navbar.Collapse>  
           </Navbar>
         </Container>
       </div>
